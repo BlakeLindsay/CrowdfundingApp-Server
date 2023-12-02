d883237c2b7c3a15d5147986169d1d4287f022e5
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-12-02 13:27:47 -0700

    donation progress

--- controllers/donation.controller.js
+++ controllers/donation.controller.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 
-router.post();
+router.get("/getaccesstoken", async (req, res) => {
+	try {
+		const accessToken = await fetch("https://api-m.sandbox.paypal.com/v1/oauth2/token", {
+			headers: new Headers({
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			method: 'POST'
+		});
+	} catch (error) {
+		res.status(500).json({
+			ERROR: error.message
+		})
+	}
+});
 
 module.exports = router;
