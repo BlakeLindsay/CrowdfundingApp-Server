449a206c9afa0fe193814c22d7598de5de1d0367
Alexander Zic <zic.alexander@gmail.com>
2023-12-04 23:46:46 -0700

    update campaign photo not working

--- controllers/campaign.controller.js
+++ controllers/campaign.controller.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const Campaign = require('../models/campaign.model');
-const validateSession = require('../middleware/validateSession');
-const uploadURL = require('../s3.js');
+const router = require("express").Router();
+const Campaign = require("../models/campaign.model");
+const validateSession = require("../middleware/validateSession");
+const uploadURL = require("../s3.js");
 
-router.post('/create',validateSession, async function (req, res) {
+router.post("/create", validateSession, async function (req, res) {
   try {
     const {
       campaignName,
@@ -11,9 +11,9 @@ router.post('/create',validateSession, async function (req, res) {
       campaignType,
       shortDesc,
       detailDesc,
-      campaignImageLink
+      campaignImageLink,
     } = req.body;
-// saving user
+    // saving user
     const owner = req.user.userName;
 
     // Save campaign to the database
@@ -29,19 +29,19 @@ router.post('/create',validateSession, async function (req, res) {
     });
     //save new campaign
     const madeCampaign = await newCampaign.save();
-     // Return a success response with loggedInUser information
-     return res.status(200).json({
+    // Return a success response with loggedInUser information
+    return res.status(200).json({
       madeCampaign,
       loggedInUser: res.locals.loggedInUser,
-      message: 'Campaign created successfully',
+      message: "Campaign created successfully",
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
-router.get('/:campaignId', async function (req, res) {
+router.get("/:campaignId", async function (req, res) {
   try {
     const campaignId = req.params.campaignId;
 
@@ -52,69 +52,109 @@ router.get('/:campaignId', async function (req, res) {
     return res.status(200).json({ campaign });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Internal server error'});
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 //! Campaign Picture Link
 
 router.get("/campaignimage/makeurl", validateSession, async (req, res) => {
-	try {
-		const url = await uploadURL();
-
-		res.status(200).json({
-			url
-		});
-	} catch (error) {
-		res.status(500).json({
-			ERROR: error.message
-		});
-	}
-});
-
-router.get("/campaignimage/geturl", validateSession, async (req, res) => {
-	try {
-		const campaignId = req.campaign._id;
-		const campaign = await Campaign.findById(campaignId);
-
-		res.status(200).json({
-			url: campaign.campaignImageLink
-		});
-	} catch (error) {
-		res.status(500).json({
-			ERROR: error.message
-		});
-	}
-});
-
-router.post("/campaignimage/saveurl", validateSession, async (req, res) => {
-	try {
-		const { url } = req.body;
-		const campaignId = req.campaign._id;
-		const editedCampaign = await Campaign.findByIdAndUpdate(campaignId, {campaignImageLink: url}, {new: true});
-
-		res.status(200).json({
-			editedCampaign,
-			url
-		});
-	} catch (error) {
-		res.status(500).json({
-			ERROR: error.message
-		});
-	}
-});
-
-router.delete('/delete/:campaignId', validateSession, async function (req, res) {
   try {
-    const campaignId = req.params.campaignId;
-    // Delete the campaign
-    await Campaign.findByIdAndDelete(campaignId);
+    const url = await uploadURL();
 
-    return res.status(200).json({ message: 'Campaign deleted successfully' });
+    res.status(200).json({
+      url,
+    });
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({
+      ERROR: error.message,
+    });
   }
 });
 
+router.get("/campaignimage/geturl", validateSession, async (req, res) => {
+  try {
+    const campaignId = req.campaign._id;
+    const campaign = await Campaign.findById(campaignId);
+
+    res.status(200).json({
+      url: campaign.campaignImageLink,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ERROR: error.message,
+    });
+  }
+});
+
+router.post("/campaignimage/saveurl", validateSession, async (req, res) => {
+  try {
+    const { url } = req.body;
+    const campaignId = req.campaign._id;
+    const editedCampaign = await Campaign.findByIdAndUpdate(
+      campaignId,
+      { campaignImageLink: url },
+      { new: true }
+    );
+
+    res.status(200).json({
+      editedCampaign,
+      url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ERROR: error.message,
+    });
+  }
+});
+
+router.delete(
+  "/delete/:campaignId",
+  validateSession,
+  async function (req, res) {
+    try {
+      const campaignId = req.params.campaignId;
+      // Delete the campaign
+      await Campaign.findByIdAndDelete(campaignId);
+
+      return res.status(200).json({ message: "Campaign deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  }
+);
+
+router.put("/update/:campaignId", validateSession, async function (req, res) {
+  try {
+    const campaignId = req.params.campaignId;
+    const updateData = req.body;
+
+    // Fetch the campaign by ID
+    const campaign = await Campaign.findById(campaignId);
+
+    if (!campaign) {
+      return res.status(404).json({ message: "Campaign not found" });
+    }
+
+    // Update campaign fields
+    campaign.campaignName = updateData.campaignName || campaign.campaignName;
+    campaign.fundGoal = updateData.fundGoal || campaign.fundGoal;
+    campaign.campaignType = updateData.campaignType || campaign.campaignType;
+    campaign.shortDesc = updateData.shortDesc || campaign.shortDesc;
+    campaign.detailDesc = updateData.detailDesc || campaign.detailDesc;
+    campaign.campaignImageLink =
+      updateData.campaignImageLink || campaign.campaignImageLink;
+
+    // Save the updated campaign
+    const updatedCampaign = await campaign.save();
+
+    return res
+      .status(200)
+      .json({ updatedCampaign, message: "Campaign updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
